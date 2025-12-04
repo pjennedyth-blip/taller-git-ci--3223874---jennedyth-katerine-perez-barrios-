c88bc2306d752ca2de43d48d6a5c00806b3e5c51
jennedyth perez <pjennedyth@gmail.com>
2025-12-04 07:27:21 -0500

    feat(logic): función agregarItem para añadir elementos

--- src/app.js
+++ src/app.js
@@ -2,6 +2,14 @@ function suma(a, b) {
   return a + b;
 }
 
+// NUEVA FUNCIÓN PARA LA PARTE 3
+function agregarItem(texto) {
+  const lista = document.getElementById("lista");
+  const li = document.createElement("li");
+  li.textContent = texto;
+  lista.appendChild(li);
+}
+
 console.log("App lista");
 
-module.exports = { suma };
+module.exports = { suma, agregarItem };
